75e1bb4a96a242130020a7b049c0877bb7ece311
Lleyton <lagenorwoth@gmail.com>
2025-10-24 23:14:29 +0300

    updating hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
@@ -9,9 +10,14 @@ export const Home = () => {
 
         {/* Background Effect */}
         <StarBackground />
+
         {/* NavBar */}
         <Navbar />
+
         {/*  Main Content */}
+        <main>
+                <HeroSection/>
+        </main>
 
         {/* Footer */}
         </div>;
